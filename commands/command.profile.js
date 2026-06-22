6aa5d362b7ba5003b1c1ba577ca3990a2005905c
Anmol Anand <anmolcollection123@gmail.com>
2026-06-22 16:56:34 +0530

    add inventory in profile

--- commands/command.profile.js
+++ commands/command.profile.js
@@ -4,6 +4,7 @@ module.exports = (message) => {
   const { EmbedBuilder } = require("discord.js");
   const credits = player.credits.toString()
   const XP = player.xp.toString()
+  const inventory = player.inventory
 
   const profileEmbed = new EmbedBuilder()
     .setTitle("👤 Player Profile")
@@ -12,6 +13,7 @@ module.exports = (message) => {
       { name: "⭐ Level", value: "1", inline: true },
       { name: "💰 Credits", value: credits, inline: true },
       { name: "📈 XP", value: XP, inline: true },
+      { name: "🎒 Inventory", value: inventory.length.toString(), inline:true}
     );
 
   message.reply({ embeds: [profileEmbed]});
