f06ffed100588f7350511a2873bf286f92bb0576
Sudo_why <anmolcollection123@gmail.com>
2026-07-07 00:58:31 +0530

    fix some bugs

--- middelware/genrateToken.js
+++ middelware/genrateToken.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (user) =>{
-const jwt_token = jwt.sign(
-  {
-    userId: user._id,
-    discordId: user.id,
-  },
-  process.env.JWT_SECRET,
-  {
-    expiresIn: "7d",
-  },
-);
-}
+module.exports = (user) => {
+  return jwt.sign(
+    {
+      userId: user._id,
+      discordId: user.id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
