6a6636099f97ceead2cc9bfbf5241d0ffb600612
Sudo_why <anmolcollection123@gmail.com>
2026-07-06 10:25:53 +0530

    try to fix some fucking bug

--- middelware/auth.js
+++ middelware/auth.js
@@ -16,34 +16,43 @@ const oauth2 = new DiscordOAuth2({
 
 // Login route
 router.get("/auth/discord", async (req, res) => {
-  const url = await oauth2.GenerateOAuth2Url({
-    state: StateTypes.UserAuth,
-    scope: [Scopes.Identify],
-  }).then((result) => {
-    console.log(result);
-    router.get("/auth/discord/callback", (req, res) => {
-      const { code } = req.query;
-      console.log(code);
-      
-        oauth2.GetAccessToken(code).then((result2) => {
-          console.log("Access Token", result2);
+  const url = await oauth2
+    .GenerateOAuth2Url({
+      state: StateTypes.UserAuth,
+      scope: [Scopes.Identify],
+    })
+    .then((result) => {
+      console.log(result);
+      router.get("/auth/discord/callback", (req, res) => {
+        const { code } = req.query;
+        console.log(code);
 
-          res.send(`you sucessfull login click on this link <a href="http://localhost:5173">Visit Google</a>`);
+        oauth2
+          .GetAccessToken(code)
+          .then((result2) => {
+            console.log("Access Token", result2);
 
-
-        }).then((result3)=>{
-            oauth2.UserDataSchema.GetUserProfile(result3.accessToken).then((result4) => {
-            console.log(result4);
+            res.send(
+              `you sucessfull login click on this link <a href="http://localhost:5173">Visit Google</a>`,
+            );
+          })
+          .then((result3) => {
+            oauth2.UserDataSchema.GetUserProfile(result3.accessToken).then(
+              (result4) => {
+                console.log(result4);
+              },
+            );
           });
-        })
-    });
-    res.redirect("https://discord.com/oauth2/authorize?client_id=1516643856022507580&response_type=code&redirect_uri=https%3A%2F%2Fcyber-rpg-production.up.railway.app%2Fauth%2Fdiscord%2Fcallback&scope=identify");
+      });
+      res.redirect(
+        url
+      );
 
       console.log(user);
 
-  res.send("Login successful");
-  });
-}) 
+      res.send("Login successful");
+    });
+});
 
 // Callback
 // router.get("/auth/discord/callback", async(req, res) => {
