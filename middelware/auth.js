5be931404bb8cdb3fdabd686d258a8779e40fe91
Sudo_why <anmolcollection123@gmail.com>
2026-07-06 11:50:24 +0530

    try to fix some fucking bug

--- middelware/auth.js
+++ middelware/auth.js
@@ -16,52 +16,29 @@ const oauth2 = new DiscordOAuth2({
 
 // Login route
 router.get("/auth/discord", async (req, res) => {
-  oauth2
-    .GenerateOAuth2Url({
-      state: StateTypes.UserAuth,
-      scope: [Scopes.Identify],
-    })
-    .then((result) => {
-      console.log(result);
-
-      const url = result.url
-
-      router.get("/auth/discord/callback", (req, res) => {
-        const { code } = req.query;
-        console.log(code);
-
-        oauth2
-          .GetAccessToken(code)
-          .then((result2) => {
-            console.log("Access Token", result2);
-
-            res.send(
-              `you sucessfull login click on this link <a href="http://localhost:5173">Visit Google</a>`,
-            );
-          })
-          .then((result3) => {
-            oauth2.UserDataSchema.GetUserProfile(result3.accessToken).then(
-              (result4) => {
-                console.log(result4);
-              },
-            );
-          });
-      });
-      res.redirect(url);
+    const result = await oauth2.GenerateOAuth2Url({
+        state: StateTypes.UserAuth,
+        scope: [Scopes.Identify],
     });
+
+    res.redirect(result.url);
 });
 
-// Callback
-// router.get("/auth/discord/callback", async(req, res) => {
-//   const { code } = req.query;
+router.get("/auth/discord/callback", async (req, res) => {
 
-//   const token = await oauth2.GetAccessToken(code);
+    const { code } = req.query;
 
-//   const user = await oauth2.UserDataSchema.GetUserProfile(token.accessToken);
+    const token = await oauth2.GetAccessToken(code);
 
-//   console.log(user);
+    console.log(token);
 
-//   res.send("Login successful");
-// });
+    const user = await oauth2.UserDataSchema.GetUserProfile(
+        token.accessToken
+    );
+
+    console.log(user);
+
+    res.send("Login successful");
+});
 
 module.exports = router;
