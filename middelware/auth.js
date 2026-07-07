f42787b338b7398ab74f18d421f4928f8f2b4634
Sudo_why <anmolcollection123@gmail.com>
2026-07-07 15:40:03 +0530

    try to connect auth in frountend

--- middelware/auth.js
+++ middelware/auth.js
@@ -1,7 +1,13 @@
 const jwt = require("jsonwebtoken");
 
+
 module.exports = (req, res, next) => {
-  const token = req.cookies.token;
+  console.log("Cookies:", req.cookies);
+  console.log("Cookie Header:", req.headers.cookie);
+
+  const token = req.cookies?.token;
+
+  console.log("Token:", token);
 
   if (!token) {
     return res.status(401).json({
@@ -12,9 +18,29 @@ module.exports = (req, res, next) => {
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
-  } catch {
+  } catch (err) {
+    console.log(err);
     return res.status(401).json({
       message: "Invalid token",
     });
   }
 };
+
+// module.exports = (req, res, next) => {
+//   const token = req.cookies.token;
+
+//   if (!token) {
+//     return res.status(401).json({
+//       message: "Unauthorized",
+//     });
+//   }
+
+//   try {
+//     req.user = jwt.verify(token, process.env.JWT_SECRET);
+//     next();
+//   } catch {
+//     return res.status(401).json({
+//       message: "Invalid token",
+//     });
+//   }
+// };
