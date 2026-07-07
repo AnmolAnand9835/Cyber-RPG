beb2fe24ea802458f5ba505e9f86f82360f9e5ab
Sudo_why <anmolcollection123@gmail.com>
2026-07-07 16:25:13 +0530

    fix typo

--- middelware/auth.js
+++ middelware/auth.js
@@ -2,12 +2,7 @@ const jwt = require("jsonwebtoken");
 
 
 module.exports = (req, res, next) => {
-  console.log("Cookies:", req.cookies);
-  console.log("Cookie Header:", req.headers.cookie);
-
-  const token = req.cookies?.token;
-
-  console.log("Token:", token);
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({
@@ -18,29 +13,9 @@ module.exports = (req, res, next) => {
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
-  } catch (err) {
-    console.log(err);
+  } catch {
     return res.status(401).json({
       message: "Invalid token",
     });
   }
 };
-
-// module.exports = (req, res, next) => {
-//   const token = req.cookies.token;
-
-//   if (!token) {
-//     return res.status(401).json({
-//       message: "Unauthorized",
-//     });
-//   }
-
-//   try {
-//     req.user = jwt.verify(token, process.env.JWT_SECRET);
-//     next();
-//   } catch {
-//     return res.status(401).json({
-//       message: "Invalid token",
-//     });
-//   }
-// };
