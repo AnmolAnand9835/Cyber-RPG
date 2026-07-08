c44e820a6087090dc4d08054e7c0e73aa464a615
Sudo_why <anmolcollection123@gmail.com>
2026-07-08 15:11:47 +0530

    try to fix somthing

--- middelware/Update.js
+++ middelware/Update.js
@@ -1,18 +1,15 @@
-const player = require('../models/Player')
+const player = require("../models/Player");
 
-module.exports = async(message) => {
-    const username = message.author.username;
-const avatar = message.author.displayAvatarURL({
-  extension: "png",
-  size: 512,
-});
+module.exports = async (message) => {
+  const username = message.author.username;
+  const avatar = message.author.displayAvatarURL({
+    extension: "png",
+    size: 512,
+  });
 
-if (
-  player.username !== username ||
-  player.avatar !== avatar
-) {
-  player.username = username;
-  player.avatar = avatar;
-  await player.save();
-}
-}
+  if (player.username !== username || player.avatar !== avatar) {
+    player.username = username;
+    player.avatar = avatar;
+    await player.save();
+  }
+};
